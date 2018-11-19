bf466006394b5ea8a0a0ce409b69674ffef3e55d
Jiwon Yeom <j-yeom@oz-vision.co.jp>
2018-11-19 18:12:07 +0900

    add command

--- common/useful-commands.php
+++ common/useful-commands.php
@@ -4,3 +4,8 @@
 ```ssh
 tail -f /var/www/wasabi/runtime/logs/app.log
 ```
+
+#### Delete all local branch except master branch
+```ssh
+git branch | egrep -v "(master|\*)" | xargs git branch -D
+```
